c70e19912cf0631f6aa2c2319180dccb48f6dc89
Saksham <sakshamshinde412@gmail.com>
2024-03-17 19:12:32 +0530

    Side effects and hooks

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://sum-server.100xdevs.com/todos")
+    .then(async function(res) {
+      const json = await res.json();
+      setTodos(json.todos)
+    })
+  },[])
+  
+  return (
+    <div>
+      {todos.map(todo=> <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
+    </div>
+  )
+}
+
+function Todo({title, description}){
+  return <div>
+    <h1>{title}</h1>
+    <h3>{description}</h3>
+  </div>
+}
+function CardWrapper({children}){
+  return <div>
+    <div style={{border: "2px solid black"}}>{children}</div>
+  </div>
+}
+export default App
